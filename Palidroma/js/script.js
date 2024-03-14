58a1dae344b9f6ee8b76216cf4ead661d2810555
NeilMagon <neil.magon97@gmail.com>
2024-03-14 21:40:58 +0100

    feat: add palidroma

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -2,3 +2,25 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 // Su internet ci sono soluzioni con join e reverse, non potete usarle!
+
+// Chiedo la parola all'utente
+const userWord = prompt (`Inserisci una parola: `);
+
+let word = revertedWord(userWord);
+
+// Controllo che la parola sia palindroma
+if (word ===  userWord) {
+    alert(`La tua parola è Palidroma`)
+} else {
+    alert("La tua parola non è Palidroma")
+};
+
+// Creo la funziona per leggere la parola al contrario
+function revertedWord(word) {
+    let revertedWord = ``;
+    for (let i = userWord.length - 1; i >= 0; i--) {
+        let letter = userWord[i];
+        revertedWord += letter;
+    }
+    return revertedWord;
+}
